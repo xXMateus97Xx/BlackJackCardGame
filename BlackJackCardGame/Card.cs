7823cb30b14f13d73c9bdb4e1dad71b98a61edd6
Mateus Bernardi Marcato <mateus02_bernardi@hotmail.com>
2025-01-26 21:36:27 -0300

    Simplify card calculations

--- BlackJackCardGame/Card.cs
+++ BlackJackCardGame/Card.cs
@@ -4,9 +4,12 @@ readonly struct Card
 {
     static ReadOnlySpan<char> CardRanks => "A23456789TJQK";
 
-    public Card(char rank, CardSuit suit)
+    public Card(CardRank rank, CardSuit suit)
     {
-        if (!CardRanks.Contains(rank))
+        if (rank != CardRank.A && rank != CardRank.Two && rank != CardRank.Three && rank != CardRank.Four &&
+            rank != CardRank.Five && rank != CardRank.Six && rank != CardRank.Seven && rank != CardRank.Eight &&
+            rank != CardRank.Nine && rank != CardRank.Ten && rank != CardRank.J && rank != CardRank.Q &&
+            rank != CardRank.K)
             throw new ArgumentException($"Invalid rank {rank}");
 
         if (suit != CardSuit.Clubs && suit != CardSuit.Diamonds && suit != CardSuit.Hearts && suit != CardSuit.Spades)
@@ -16,21 +19,40 @@ readonly struct Card
         Suit = suit;
     }
 
-    public char Rank { get; }
+    public CardRank Rank { get; }
     public CardSuit Suit { get; }
 
-    public int IntegerValue => Math.Min(CardRanks.IndexOf(Rank) + 1, 10);
+    public int IntegerValue => ((short)Rank & 0xF0) >> 4;
 
-    public override string ToString() => $"{Rank}{(char)Suit}";
+    public override string ToString() => $"{CardRanks[(short)Rank & 0xF]}{(char)Suit}";
 
     public void WriteToSpan(Span<char> destination)
     {
         if (destination.Length > 1)
         {
-            destination[0] = Rank;
+            destination[0] = CardRanks[(short)Rank & 0xF];
             destination[1] = (char)Suit;
         }
     }
+}
+
+/* Os 4 bits mais significativos guardam o valor inteiro da carta
+   Os 4 bits menos significativos guardam a posição da letra no CardRanks */
+enum CardRank : short
+{
+    A = 1 << 4,
+    Two = 1 | 2 << 4,
+    Three = 2 | 3 << 4,
+    Four = 3 | 4 << 4,
+    Five = 4 | 5 << 4,
+    Six = 5 | 6 << 4,
+    Seven = 6 | 7 << 4,
+    Eight = 7 | 8 << 4,
+    Nine = 8 | 9 << 4,
+    Ten = 9 | 10 << 4,
+    J = 10 | 10 << 4,
+    Q = 11 | 10 << 4,
+    K = 12 | 10 << 4,
 }
 
 enum CardSuit : short
